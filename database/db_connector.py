adbca11acfdcf083ba66e55272f8f1983e295fdd
beteivap <beteivap@oregonstate.edu>
2021-02-10 18:45:39 -0800

    updated to use dotenv variables

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,16 +1,15 @@
 import MySQLdb
 import os
-# from dotenv import load_dotenv, find_dotenv
-from database.db_credentials import host, user, passwd, db
+from dotenv import load_dotenv, find_dotenv
 
 # Load our environment variables from the .env file in the root of our project.
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
 # Set the variables in our application with those environment variables
-# host = os.environ.get("340DBHOST")
-# user = os.environ.get("340DBUSER")
-# passwd = os.environ.get("340DBPW")
-# db = os.environ.get("340DB")
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+passwd = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
 
 
 def connect_to_database(host=host, user=user, passwd=passwd, db=db):
