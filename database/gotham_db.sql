9e03b7b14f9e06406f2be30c6983f802f2d1be79
beteivap <beteivap@oregonstate.edu>
2021-02-22 20:32:03 -0800

    updated queries to capitalize table names

--- database/gotham_db.sql
+++ database/gotham_db.sql
@@ -1,4 +1,6 @@
-CREATE TABLE users (
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
     userID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
     userName VARCHAR(255) UNIQUE NOT NULL ,
     password VARCHAR(255) NOT NULL,
@@ -9,12 +11,14 @@ CREATE TABLE users (
     PRIMARY KEY (userID)
 );
 
-INSERT INTO users (userName, password, firstName, lastName, email, dateJoined) VALUES 
+INSERT INTO Users (userName, password, firstName, lastName, email, dateJoined) VALUES 
     ('johndoe', 'password1', 'John', 'Doe', 'johndoe@gmail.com', '2020-04-04'),
     ('janedoe', 'password2', 'Jane', 'Doe', 'janedoe@gmail.com', '2019-04-04'),
     ('jimdoe', 'password3', 'Jim', 'Doe', 'jimdoe@gmail.com', '2018-04-04');
 
-CREATE TABLE listings (
+DROP TABLE IF EXISTS Listings;
+
+CREATE TABLE Listings (
     listingID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
     userID INT(11),
     bidID INT(11),
@@ -26,67 +30,75 @@ CREATE TABLE listings (
     listDate DATE NOT NULL,
     expirationDate DATE NOT NULL,
     PRIMARY KEY (listingID),
-    FOREIGN KEY (userID) REFERENCES users(userID)
+    FOREIGN KEY (userID) REFERENCES Users(userID)
 );
 
-INSERT INTO listings (userID, bidID, make, model, year, mileage, reserve, listDate, expirationDate) VALUES 
+INSERT INTO Listings (userID, bidID, make, model, year, mileage, reserve, listDate, expirationDate) VALUES 
     (1, NULL, 'Ford', 'Mustang', 1972, 130000, 100000, '2020-04-04', '2020-06-06'),
     (2, NULL, 'Volvo', 'V70R', 2002, 150000, 110000, '2020-03-04', '2020-05-06'),
     (3, NULL, 'Mercedes', '300SL Gullwing', 1963, 10000, 130000, '2020-02-04', '2020-04-06'),
     (3, NULL, 'Ferrari', 'F40 GTR', 1999, 45783, 1500000, '2020-01-04', '2020-02-03');
 
-CREATE TABLE bids (
+DROP TABLE IF EXISTS Bids;
+
+CREATE TABLE Bids (
     bidID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
     userID INT(11) NOT NULL,
     listingID INT(11) NOT NULL,
     bidAmt INT(11) NOT NULL,
     bidDate DATE NOT NULL,
     PRIMARY KEY (bidID),
-    FOREIGN KEY (userID) REFERENCES users(userID),
-    FOREIGN KEY (listingID) REFERENCES listings(listingID)
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID)
 );
 
-ALTER TABLE listings
-ADD FOREIGN KEY (bidID) REFERENCES bids(bidID);
+ALTER TABLE Listings
+ADD FOREIGN KEY (bidID) REFERENCES Bids(bidID);
 
-INSERT INTO bids (userID, listingID, bidAmt, bidDate) VALUES 
+INSERT INTO Bids (userID, listingID, bidAmt, bidDate) VALUES 
     (1, 1, 20000, '2020-04-05'),
     (1, 2, 50000, '2020-03-06'),
     (2, 3, 70000, '2020-02-08'),
     (3, 1, 25000, '2020-04-06');
 
-UPDATE listings SET bidID = 4 WHERE listingID = 1;
-UPDATE listings SET bidID = 2 WHERE listingID = 2;
-UPDATE listings SET bidID = 3 WHERE listingID = 3;
+UPDATE Listings SET bidID = 4 WHERE listingID = 1;
+UPDATE Listings SET bidID = 2 WHERE listingID = 2;
+UPDATE Listings SET bidID = 3 WHERE listingID = 3;
 
-CREATE TABLE features (
+DROP TABLE IF EXISTS Features;
+
+CREATE TABLE Features (
     featureID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
     carFeature VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (featureID)
 );
 
-INSERT INTO features (carFeature) VALUES ('Manual'), ('Electric'), ('Convertible'), ('4WD');
+INSERT INTO Features (carFeature) VALUES ('Manual'), ('Electric'), ('Convertible'), ('4WD');
 
-CREATE TABLE photos (
+DROP TABLE IF EXISTS Photos;
+
+CREATE TABLE Photos (
     photoID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
     listingID INT(11) NOT NULL,
     photoPath VARCHAR(255) NOT NULL,
     PRIMARY KEY (photoID),
-    FOREIGN KEY (listingID) REFERENCES listings(listingID)
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID)
 );
 
-INSERT INTO photos (listingID, photoPath) VALUES
+INSERT INTO Photos (listingID, photoPath) VALUES
     (1, 'static/img/test_car_img.jpg'),
     (2, 'static/img/test_car_img.jpg'),
     (3, 'static/img/test_car_img.jpg'),
     (4, 'static/img/test_car_img.jpg');
 
+DROP TABLE IF EXISTS FeaturesListings;
+
 CREATE TABLE FeaturesListings (
     listingID INT(11),
     featureID INT(11),
     PRIMARY KEY (listingID, featureID),
-    FOREIGN KEY (listingID) REFERENCES listings(listingID),
-    FOREIGN KEY (featureID) REFERENCES features(featureID)
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID),
+    FOREIGN KEY (featureID) REFERENCES Features(featureID)
 );
 
 INSERT INTO FeaturesListings (listingID, featureID) VALUES
