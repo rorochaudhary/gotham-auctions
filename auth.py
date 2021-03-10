3d047e30b8a447d0348defa54cfa7ee0eb3bd15d
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2021-03-09 16:41:59 -0800

    successful account creation redirects to home+logged in

--- auth.py
+++ auth.py
@@ -46,7 +46,15 @@ def register():
                 # storing hashed password for simplicity
                 # if no-hash preferred, directly insert password into query
             )
-            return redirect(url_for('auth.login'))
+
+            # redirect to home with user logged in
+            user = db.execute_query(db_conn,
+            'SELECT * FROM Users WHERE userName = %s',
+            (username,)).fetchone()
+            session.clear()
+            session['user_id'] = user['userID']
+            
+            return redirect(url_for('root'))
 
         flash(error)
 
